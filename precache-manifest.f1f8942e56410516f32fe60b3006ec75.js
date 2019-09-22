a8c086e1ed820e754b8c602bad50196c340be24f
Yash Agrawal <agrawalyash1919@gmail.com>
2019-09-22 14:36:39 +0530

    Updates

---
+++ precache-manifest.f1f8942e56410516f32fe60b3006ec75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d03217d7fda5a3d5f19eb45df939da",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "4e3c29166331d9305088",
+    "url": "/ReactApp/static/css/main.41ad34c3.chunk.css"
+  },
+  {
+    "revision": "74603a913ba996464b28",
+    "url": "/ReactApp/static/js/2.9e7efe27.chunk.js"
+  },
+  {
+    "revision": "4e3c29166331d9305088",
+    "url": "/ReactApp/static/js/main.c3b56d81.chunk.js"
+  },
+  {
+    "revision": "f2d93e315843cf6fce0e",
+    "url": "/ReactApp/static/js/runtime-main.9e6d8806.js"
+  }
+]);
