f3bcda566e31a8aeae6fe12345960a933b356a01
Yash Agrawal <agrawalyash1919@gmail.com>
2019-09-22 15:09:29 +0530

    Deploy Fix 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Display from './Search/Display'
 import Compare from './Compare/Compare'
 import Navbar from './Navbar'
@@ -7,13 +7,13 @@ import Navbar from './Navbar'
 class App extends Component {
   render() {
     return (
-      <HashRouter basename="/ReactApp">
+      <BrowserRouter>
         <Navbar />
         <div>
           <Route path='/' component={Display} exact />
           <Route path='/compare' component={Compare} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 }
